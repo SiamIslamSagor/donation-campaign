44e3b9ad3eb522de533a17d94784973f401e1f24
SiamIslamSagor <msiamislam12345@gmail.com>
2023-09-25 20:29:20 +0600

    added See All button functionality in DonationBox element

--- src/Components/DonationBox/DonationBox.jsx
+++ src/Components/DonationBox/DonationBox.jsx
@@ -1,10 +1,11 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { JsonDataContext } from "../Home/Home";
 import { getDonatedDataInLC } from "../../localStorage";
 import DonateCard from "../DonateCard/DonateCard";
 
 const DonationBox = () => {
   const data = useContext(JsonDataContext);
+  const [donatedDataLength, setDonatedDataLength] = useState(4);
   //   console.log(data);
   const donatedDataIds = getDonatedDataInLC();
   const donatedData = data.filter(
@@ -12,14 +13,26 @@ const DonationBox = () => {
   );
   console.log(donatedData);
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto my-20">
       <div className="grid justify-center gap-5 items-center  grid-cols-1 xl:grid-cols-2">
-        {donatedData.map(singleDonate => (
+        {donatedData.slice(0, donatedDataLength).map(singleDonate => (
           <DonateCard
             singleDonate={singleDonate}
             key={singleDonate.id}
           ></DonateCard>
         ))}
+      </div>
+      <div
+        className={`text-center my-14 ${donatedData.length <= 4 && "hidden"} ${
+          donatedData.length === donatedDataLength && "hidden"
+        }`}
+      >
+        <button
+          onClick={() => setDonatedDataLength(donatedData.length)}
+          className="btn btn-success text-white text-lg px-8"
+        >
+          See All
+        </button>
       </div>
     </div>
   );
