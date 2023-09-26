6f3d632fcbce81eb8f85e28cf49dab34fa2827c3
SiamIslamSagor <msiamislam12345@gmail.com>
2023-09-26 17:12:50 +0600

    change error page style like netlify

--- src/Components/ErrorPage/ErrorPage.jsx
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -1,22 +1,24 @@
 import { Link } from "react-router-dom";
-import Header from "../Header/Header";
 import { IoChevronBackOutline } from "react-icons/io5";
 
 const ErrorPage = () => {
   return (
     <div>
-      <Header></Header>
-      <div className=" h-[70vh] flex flex-col justify-center items-center gap-5">
-        <h2 className="text-5xl font-semibold text-center">
-          Ops <span className="text-red-600">!!!</span>
-        </h2>
-        <p className="text-2xl text-red-600">Something is wrong!</p>
-        <p className="text-lg text-gray-500">Page Not Found.</p>
-        <Link to={"/"}>
-          <button className="btn btn-outline btn-link duration-700 font-bold text-blue-700">
-            <IoChevronBackOutline></IoChevronBackOutline>Go Home
-          </button>
-        </Link>
+      <div className="h-[100vh] bg-gray-100 flex justify-center items-center">
+        <div className="shadow-lg px-20 py-5 bg-white rounded-md flex flex-col justify-center items-center gap-5">
+          <h2 className="text-5xl font-semibold text-center">
+            Ops <span className="text-red-600">!!!</span>
+          </h2>
+          <p className="text-2xl text-center text-red-600">
+            Something is wrong!
+          </p>
+          <p className="text-lg text-center text-gray-500">Page Not Found.</p>
+          <Link to={"/"}>
+            <button className="btn btn-outline btn-link duration-700 font-bold text-blue-700">
+              <IoChevronBackOutline></IoChevronBackOutline>Go Home
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
