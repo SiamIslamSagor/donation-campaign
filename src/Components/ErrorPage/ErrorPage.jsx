1f6f9fb19042fe3ffe4fa3d25fe2ee0909bb5a9f
SiamIslamSagor <msiamislam12345@gmail.com>
2023-09-26 16:54:04 +0600

    modify error page style

--- src/Components/ErrorPage/ErrorPage.jsx
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -1,15 +1,21 @@
 import { Link } from "react-router-dom";
 import Header from "../Header/Header";
+import { IoChevronBackOutline } from "react-icons/io5";
 
 const ErrorPage = () => {
   return (
     <div>
       <Header></Header>
-      <div className=" h-screen flex flex-col justify-center items-center gap-5">
-        <h2 className="text-5xl text-center">Ops!!</h2>
-        <p className="text-2xl">Page Not Found</p>
-        <Link>
-          <button className="btn btn-primary">Go Home</button>
+      <div className=" h-[70vh] flex flex-col justify-center items-center gap-5">
+        <h2 className="text-5xl font-semibold text-center">
+          Ops <span className="text-red-600">!!!</span>
+        </h2>
+        <p className="text-2xl text-red-600">Something is wrong!</p>
+        <p className="text-lg text-gray-500">Page Not Found.</p>
+        <Link to={"/"}>
+          <button className="btn btn-outline btn-link duration-700 font-bold text-blue-700">
+            <IoChevronBackOutline></IoChevronBackOutline>Go Home
+          </button>
         </Link>
       </div>
     </div>
